1c6c466115a9f7be2182127509d896764f75ae09
Yuriy Peskov <yuriy.peskov@gmail.com>
2021-02-27 17:17:22 +0200

    refactored to call user and post creation independently of other entities of the same type

--- db_homework/app.py
+++ db_homework/app.py
@@ -16,36 +16,50 @@ async def add_users_if_required():
     await user.create()
 
 
+async def create_user(u: User):
+    user = User(id=u['id'],
+                name=u['name'],
+                username=u['username'],
+                email=u['email'],
+                phone=u['phone'])
+    try:
+        await user.create()
+        logger.info(f'User {user.username} created')
+    except UniqueViolationError as e:
+        print(f'Skipping duplicate: [{user.username}]')
+
+
 async def get_users_and_save():
     logger.info('Starting fetching users')
     users = await fetch_data(url='https://jsonplaceholder.typicode.com/users')
     # print(json.dumps(users, indent=4))
+
+    user_lst = []
     for u in users:
-        user = User(id=u['id'],
-                    name=u['name'],
-                    username=u['username'],
-                    email=u['email'],
-                    phone=u['phone'])
-        try:
-            await user.create()
-            logger.info(f'User {user.username} created')
-        except UniqueViolationError as e:
-            print(f'Skipping duplicate: [{user.username}]')
+        user_lst.append(create_user(u))
+    await asyncio.gather(*user_lst)
 
     logger.info('All users are created')
+
+
+async def create_post(p):
+    post = Post(id=p['id'],
+                user_id=p['userId'],
+                title=p['title'],
+                body=p['body'])
+    await post.create()
+    logger.info(f'Post {post.title} is created')
 
 
 async def get_posts_and_save():
     logger.info('Starting fetch posts')
     posts = await fetch_data(url='https://jsonplaceholder.typicode.com/posts')
     # print(json.dumps(posts, indent=4))
+
+    posts_lst = []
     for p in posts:
-        post = Post(id=p['id'],
-                    user_id=p['userId'],
-                    title=p['title'],
-                    body=p['body'])
-        await post.create()
-        logger.info(f'Post {post.title} is created')
+        posts_lst.append(create_post(p))
+    await asyncio.gather(*posts_lst)
 
     logger.info('All posts are created')
 
