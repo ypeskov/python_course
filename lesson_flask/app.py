b5c0e535420c466ca85015b857586d69578facb8
Yuriy Peskov <yuriy.peskov@gmail.com>
2021-03-03 11:59:37 +0200

    finished first web app

--- lesson_flask/app.py
+++ lesson_flask/app.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template
 
+from views.auth.auth import auth_app
+
 app = Flask(__name__)
 
+app.register_blueprint(auth_app, url_prefix='/auth')
 
-@app.route('/', methods=['GET'])
+
+@app.route('/', methods=['GET'], endpoint='home')
 def index():
     return render_template('index.html')
-
-
-@app.route('/hello/<name>')
-def hello(name):
-    return f'Hello, {name}'
 
 
 if __name__ == '__main__':
--- lesson_flask/static/js/index.js
+++ lesson_flask/static/js/index.js
@@ -1,7 +1,15 @@
 "use strict";
 
-let loginButton = document.getElementById('go-login');
+const loginButton = document.getElementById('go-login');
+const infoMsg = document.querySelector('.info-msg');
+const logoutButton = document.querySelector('#logout');
+
 loginButton.addEventListener('click', (event) => {
-    alert('prepare for redirect');
-    document.location = `/login-page`;
+    loginButton.classList.add('hidden');
+    infoMsg.classList.remove('hidden');
+});
+
+logoutButton.addEventListener('click', () => {
+    infoMsg.classList.add('hidden');
+    loginButton.classList.remove('hidden');
 });
