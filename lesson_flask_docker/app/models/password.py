6afebf5a3f277ac3fe3cb470335362a8a9109035
Yuriy Peskov <yuriy.peskov@gmail.com>
2021-03-13 21:33:25 +0200

    changed model, changed html for list

--- lesson_flask_docker/app/models/password.py
+++ lesson_flask_docker/app/models/password.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String
 
 from .database import db
 
 
 class Password(db.Model):
     id = Column(Integer, primary_key=True)
-    resource_name = Column(String(100), unique=False, nullable=True)
+    resource_name = Column(String(100), unique=False, nullable=False, default=True)
     url = Column(String, nullable=False, default='')
     login = Column(String, nullable=False, default='')
     password = Column(String, nullable=False, default='')
-    comment = Column(String, nullable=True)
+    comment = Column(String, nullable=False, default='')
--- lesson_flask_docker/app/views/pages/pages.py
+++ lesson_flask_docker/app/views/pages/pages.py
@@ -3,17 +3,19 @@ from flask import Blueprint, render_template
 from models.database import db
 from models.password import Password
 
-
 pages_app = Blueprint('pages_app', __name__)
 
+url_home = 'home'
+url_list_passwords = 'list-passwords'
 
-@pages_app.route('/', methods=['GET'], endpoint='home')
+
+@pages_app.route('/', methods=['GET'], endpoint=url_home)
 def home():
-    return render_template('index.html', active_page='home')
+    return render_template('pages/index.html', active_page=url_home)
 
 
-@pages_app.route('/list-passwords', methods=['GET'], endpoint='list-passwords')
+@pages_app.route('/list-passwords', methods=['GET'], endpoint=url_list_passwords)
 def pages():
     passwords = Password.query.all()
 
-    return render_template('pages/list.html', passwords=passwords)
+    return render_template('pages/list.html', passwords=passwords, active_page=url_list_passwords)
